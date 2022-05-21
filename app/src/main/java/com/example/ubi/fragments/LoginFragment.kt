5b9695692c73bda133b76773cebf8f503f469d95
PawelZ4krzewski <61842320+PawelZ4krzewski@users.noreply.github.com>
2022-05-21 16:40:09 +0200

    initial commit

---
+++ app/src/main/java/com/example/ubi/fragments/LoginFragment.kt
@@ -0,0 +1,23 @@
+package com.example.ubi.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.ubi.R
+
+
+class LoginFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+}
